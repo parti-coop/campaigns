80be7fd8d4d63d1ffae3860263ff9363bd37351d
wagurano <wagurano@gmail.com>
2021-07-15 23:19:07 +0900

    가져올 최근 서명 개수를 정해야 합니다.
    Co-authored-by: Kim youngmin <dalikim@users.noreply.github.com>

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::CampaignsController < ApplicationController
 
   def signs_recent
     campaign = Campaign.find(params[:id])
-    max_count = 8
-    max_count = [params[:max_count].to_i, 32].min if params[:max_count].present?
+    max_count = [params[:max_count]&.to_i, 32].min
     signs = campaign.signs.present.recent.take(max_count).map{ |x| { signer_name: x.user_name, body: x.body, signed_at: x.created_at, sequence_no: campaign.signs.where('id <= ?', x).count } }
     render json: signs
   end
